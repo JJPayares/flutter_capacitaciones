8e8aa4c75b2294c38b38c36563acc5f14e6337e4
Juan Jose Payares <jpayares@lsv-tech.com>
2024-07-24 10:00:47 -0500

    sirve

--- lib/modules/user_profile/ui/pages/user_profile.dart
+++ lib/modules/user_profile/ui/pages/user_profile.dart
@@ -21,146 +21,147 @@ class _UserProfileState extends State<UserProfile> {
     return Scaffold(
       backgroundColor: Colors.blueAccent[700],
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.51,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Row(
-                      children: [
-                        Icon(Icons.arrow_back, color: Colors.white),
-                      ],
-                    ),
-                    const SizedBox(height: 16),
-                    const CircleAvatar(
-                      radius: 90,
-                      backgroundImage: NetworkImage(
-                          'https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-                    ),
-                    const SizedBox(height: 8),
-                    const Text(
-                      'Stephany Lebrun',
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                // height: MediaQuery.of(context).size.height * 0.51,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Row(
+                        children: [
+                          Icon(Icons.arrow_back, color: Colors.white),
+                        ],
                       ),
-                    ),
-                    const Text(
-                      'Photographer',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white,
+                      const SizedBox(height: 16),
+                      const CircleAvatar(
+                        radius: 90,
+                        backgroundImage: NetworkImage(
+                            'https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
                       ),
-                    ),
-                    const SizedBox(height: 16),
-                    const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              '150',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              'Posts',
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
+                      const SizedBox(height: 8),
+                      const Text(
+                        'Stephany Lebrun',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Column(
-                          children: [
-                            Text(
-                              '834',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              'Followers',
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
+                      ),
+                      const Text(
+                        'Photographer',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
                         ),
-                        Column(
-                          children: [
-                            Text(
-                              '341',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
+                      ),
+                      const SizedBox(height: 16),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                '150',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                            Text(
-                              'Following',
-                              style: TextStyle(
-                                color: Colors.white,
+                              Text(
+                                'Posts',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 16),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
+                            ],
                           ),
-                          child: const Text('Follow'),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
+                          Column(
+                            children: [
+                              Text(
+                                '834',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                'Followers',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
                           ),
-                          child: const Text('Message'),
-                        ),
-                      ],
-                    ),
-                  ],
+                          Column(
+                            children: [
+                              Text(
+                                '341',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                'Following',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            child: const Text('Follow'),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            child: const Text('Message'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Expanded(
-              child: LayoutBuilder(
+              const SizedBox(height: 10),
+              LayoutBuilder(
                 builder: (context, constraints) {
                   int crossAxisCount = 3;
                   double width = constraints.maxWidth;
 
                   if (width < 600) {
-                    crossAxisCount = 2;
-                  } else if (width < 900) {
                     crossAxisCount = 3;
-                  } else {
+                  } else if (width < 900) {
                     crossAxisCount = 4;
+                  } else {
+                    crossAxisCount = 5;
                   }
 
                   return GridView.builder(
+                    shrinkWrap: true,
                     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: crossAxisCount,
                       crossAxisSpacing: 4.0,
@@ -176,8 +177,8 @@ class _UserProfileState extends State<UserProfile> {
                   );
                 },
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
