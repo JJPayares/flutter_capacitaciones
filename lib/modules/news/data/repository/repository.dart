73ad0cb7b507780e3276bb723ea66855c23e4451
Juan Jose Payares <jpayares@lsv-tech.com>
2024-08-02 17:56:52 -0500

    Progress

--- lib/modules/news/data/repository/repository.dart
+++ lib/modules/news/data/repository/repository.dart
@@ -6,11 +6,8 @@ import 'package:flutter_capacitaciones/modules/news/data/models/news.dart';
 Future<List<Noticia>> cargarNoticias(int? categoryId) async {
   final String response = await rootBundle
       .loadString('lib/modules/news/data/repository/news_data.json');
-
   final Map<String, dynamic> data = json.decode(response);
-
   final List<dynamic> noticiasJson = data['noticias'];
-
   return noticiasJson
       .map((json) => Noticia.fromJson(json))
       .where((noticia) => categoryId == null || noticia.categoria == categoryId)
